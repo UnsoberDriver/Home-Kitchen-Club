2a78feefbacd7b53bd2ef1af77f5ad23b3a6015c
Unsober driver <nicolas.boulloud410@gmail.com>
2026-07-20 11:08:32 +0200

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,61 @@
+<?php
+return [
+    // Navigation / header
+    'nav_connexion' => 'Log in',
+    'nav_inscription' => 'Sign up',
+    'nav_toutes' => 'All',
+    'nav_compte' => 'Account',
+
+    // Home
+    'accueil_titre' => 'Latest recipes',
+    'accueil_resultats' => 'results',
+    'accueil_aucune_recette' => 'No recipes yet.',
+    'accueil_remonter' => 'Back to top',
+
+    // Recipe page
+    'recette_temps_total' => 'Total time',
+    'recette_portions' => 'Servings',
+    'recette_difficulte' => 'Difficulty',
+    'recette_ingredients' => 'Ingredients',
+    'recette_preparation' => 'Preparation',
+    'recette_pers' => 'servings',
+    'recette_moins_portions' => 'Fewer servings',
+    'recette_plus_portions' => 'More servings',
+    'recette_retour' => '← Back to recipes',
+
+    // Login / register
+    'login_titre' => 'Log in',
+    'login_email' => 'Email',
+    'login_mot_de_passe' => 'Password',
+    'login_rester_connecte' => 'Stay logged in',
+    'login_bouton' => 'Log in',
+    'login_pas_de_compte' => "Don't have an account yet?",
+    'login_sinscrire' => 'Sign up',
+    'login_erreur' => 'Incorrect email or password.',
+
+    'register_titre' => 'Create an account',
+    'register_nom' => 'Name',
+    'register_mdp_confirm' => 'Confirm password',
+    'register_bouton' => 'Sign up',
+    'register_deja_compte' => 'Already have an account?',
+    'register_se_connecter' => 'Log in',
+
+    // Contact
+    'contact_titre' => 'Contact us',
+    'contact_intro' => "A question, a recipe suggestion, an issue with the site? Write to us.",
+    'contact_nom' => 'Name',
+    'contact_email' => 'Email',
+    'contact_sujet' => 'Subject',
+    'contact_message' => 'Message',
+    'contact_envoyer' => 'Send',
+    'contact_annuler' => 'Cancel',
+    'contact_retour' => '← Back to home',
+
+    // Footer
+    'footer_contact' => 'Contact us',
+    'footer_mentions' => 'Legal notice',
+
+    // Legal notice
+    'mentions_titre' => 'Legal notice',
+    'mentions_maj' => 'Last updated',
+];
